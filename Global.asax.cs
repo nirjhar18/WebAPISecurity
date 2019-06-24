eba992ce00e9267d1233f04c182ae96ca0c4aacb
Nirjhar Vermani <nirjhar18@gmail.com>
2019-06-23 19:32:50 -0500

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WebApiSecurity.MessageHandlers;
+
+namespace WebApiSecurity
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //When request comes in, it will call this Message Handler
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new ApiKeyMessageHandler());
+        }
+    }
+}
