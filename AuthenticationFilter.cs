ae4151eabd5a4fca3f9c55762a58666c31c5b710
Nirjhar Vermani <nirjhar18@gmail.com>
2019-06-23 19:37:11 -0500

    Authentication Filter Class

---
+++ AuthenticationFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;.
+
+namespace WebApiSecurity
+{
+    public class AuthenticationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            //Check if header exists
+            if(actionContext.Request.Headers.Authorization == null)
+            {
+                actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            }
+            else
+            {
+                string authenticationToken = actionContext.Request.Headers.Authorization.Parameter;
+                //Base 64 Decoding
+                string decodedToken = Encoding.UTF8.GetString(Convert.FromBase64String(authenticationToken));
+                string userName = decodedToken.Substring(0, decodedToken.IndexOf(":"));
+                string userPassword = decodedToken.Substring(decodedToken.IndexOf(":")+1);
+
+                //Add your Logic to check for username and password from database
+                if(userName == "Admin" && userPassword == "Password")
+                {
+                    //Authorize - you do not have to do anything
+                }
+                else
+                {
+                    actionContext.Response = new System.Net.Http.HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+                }
+
+            }
+
+        }
+
+    }
+}
