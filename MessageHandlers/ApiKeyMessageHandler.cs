a02d480158609b62745bbeb812352b7b41a85c0c
Nirjhar Vermani <nirjhar18@gmail.com>
2019-06-23 19:34:52 -0500

    Update ApiKeyMessageHandler.cs

--- MessageHandlers/ApiKeyMessageHandler.cs
+++ MessageHandlers/ApiKeyMessageHandler.cs
@@ -13,8 +13,13 @@ using System.Web;
 //an HTTP request, does some processing, and gives the request to the next handler. At some point, the response is created and
 //goes back up the chain. This pattern is called a delegating handler.
 
-
-
+//Steps
+//1. Create a new class that inherits from delegating handler.
+//2. You can store keys in database, in this example I have a constant API key
+//3. Override the SendAsync method with parameters of Request Message and Cancellation Token
+//4. Check if API key exists in the header and then make sure API key passed by user is correct.
+//5. If everything looks good, call base SendAsync method to pass the response in the chain which will call the next method in chain.
+//
 
 namespace WebApiSecurity.MessageHandlers
 {
